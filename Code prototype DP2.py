e97340efa208be17cd5e211dffa0af2eea9fe736
leasttechnicaltustudent <M.Oosterink@student.tudelft.nl>
2024-05-31 12:34:13 +0200

    Update Code prototype DP2.py

--- Code prototype DP2.py
+++ Code prototype DP2.py
@@ -39,11 +39,13 @@ magenta_button.direction = digitalio.Direction.INPUT
 # For the Chainable LED:
 pin_leds = board.D3
 num_leds = 1
-leds = neopixel.NeoPixel(pin_leds, num_leds, auto_write=False, pixel_order=neopixel.GRBW)
+leds = neopixel.NeoPixel(
+    pin_leds, num_leds, auto_write=False, pixel_order=neopixel.GRBW
+)
 
 led_off = (0, 0, 0, 0)
-led_red = (255, 0, 0, 0)
-led_blue = (0, 0, 255, 0)
+led_red = (255, 122, 0, 0)
+led_blue = (0, 191, 255, 0)
 led_white = (0, 0, 0, 255)
 led_green = (0, 255, 0, 0)
 led_magenta = (204, 0, 204, 0)
@@ -58,6 +60,7 @@ def set_led_color(color):
     leds.fill(color)
     leds.show()
 
+
 def set_timer(duration):
     global timer_duration, timer_mark
     timer_duration = duration
@@ -70,32 +73,48 @@ def timer_expired():
     else:
         return False
 
+def set_timer2(duration):
+    global timer_duration, timer_mark
+    timer_duration = duration
+    timer_mark = time.monotonic()
+
+def timer2_expired():
+    global timer_mark, timer_duration
+    if time.monotonic() - timer_mark > timer_duration:
+        return True
+    else:
+        return False
+
 ##--- Main loop
 while True:
     if current_state == state_wait:
         set_led_color(led_off)
+        time.sleep(0.3)
         current_state = state_start_game
 
     elif current_state == state_start_game:
-        if timer_expired():
-            set_led_color(led_white)
-            set_timer(30)
-            current_state = state_wait_button_press
+        set_led_color(led_white)
+        set_timer(30)
+        current_state = state_wait_button_press
 
     elif current_state == state_wait_button_press:
         if red_button.value:
-            print("Hulp")
+            print("toilet")
+            set_timer(10)
             current_state = state_red_wins
         elif blue_button.value:
-            print("Water")
+            print("drinken")
+            set_timer(10)
             current_state = state_blue_wins
         elif green_button.value:
-            print("koffie")
+            print("eten")
+            set_timer(10)
             current_state = state_green_wins
         elif magenta_button.value:
-            print("bed")
+            print("medicijnen")
+            set_timer(10)
             current_state = state_magenta_wins
-        elif timer_expired(): 
+        elif timer_expired():
             print("Check up on patient")
             set_led_color(led_white)
             time.sleep(1)
@@ -113,24 +132,40 @@ while True:
 
     elif current_state == state_blue_wins:
         set_led_color(led_blue)
-        time.sleep(5)
-        current_state = state_onderweg_blue
+        time.sleep(0.3)
+        if timer_expired():
+            current_state = state_onderweg_blue
+        elif blue_button.value:
+            print("geannuleerd")
+            current_state = state_wait
 
     elif current_state == state_red_wins:
         set_led_color(led_red)
-        time.sleep(5)
-        current_state = state_onderweg_red
-    
+        time.sleep(0.3)
+        if timer_expired():
+            current_state = state_onderweg_red
+        elif red_button.value:
+            print("geannuleerd")
+            current_state = state_wait
+
     elif current_state == state_green_wins:
         set_led_color(led_green)
-        time.sleep(5)
-        current_state = state_onderweg_green
-        
+        time.sleep(0.3)
+        if timer_expired():
+            current_state = state_onderweg_green
+        elif green_button.value:
+            print("geannuleerd")
+            current_state = state_wait
+
     elif current_state == state_magenta_wins:
         set_led_color(led_magenta)
-        time.sleep(5)
-        current_state = state_onderweg_magenta
-        
+        time.sleep(0.3)
+        if timer_expired():
+            current_state = state_onderweg_magenta
+        elif magenta_button.value:
+            print("geannuleerd")
+            current_state = state_wait
+
     elif current_state == state_onderweg_blue:
         print("Ik kom eraan!")
         set_led_color(led_blue)
@@ -146,7 +181,7 @@ while True:
         set_led_color(led_off)
         time.sleep(1)
         current_state = state_wait
-    
+
     elif current_state == state_onderweg_red:
         print("Ik kom eraan!")
         set_led_color(led_red)
@@ -162,7 +197,7 @@ while True:
         set_led_color(led_off)
         time.sleep(1)
         current_state = state_wait
-        
+
     elif current_state == state_onderweg_green:
         print("Ik kom eraan!")
         set_led_color(led_green)
@@ -178,7 +213,7 @@ while True:
         set_led_color(led_off)
         time.sleep(1)
         current_state = state_wait
-        
+
     elif current_state == state_onderweg_magenta:
         print("Ik kom eraan!")
         set_led_color(led_magenta)
@@ -194,3 +229,5 @@ while True:
         set_led_color(led_off)
         time.sleep(1)
         current_state = state_wait
+
+
